79626a44b7aa1a5b4ccd79585ed57b8518dbc555
Yinyin Wu(Mia) <23415578@student.uwa.edu.au>
2023-08-13 19:40:54 +0800

    mockup page using user table

--- app/routes.py
+++ app/routes.py
@@ -43,6 +43,13 @@ def logout():
     logout_user()
     return redirect(url_for('homepage'))
 
+# decorator for view_workload page
+@app.route('/view_workload')
+@login_required
+def view_workload():
+    users=User.query.all() # Just using user table for mockup
+    return render_template('view_workload.html', users=users, title='View Workload')
+
 # decorator for signup page
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
