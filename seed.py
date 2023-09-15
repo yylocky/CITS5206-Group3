189f8f992eb454202dd3910db47270ae11d8e2b4
Yinyin Wu(Mia) <23415578@student.uwa.edu.au>
2023-09-15 14:09:02 +0800

    add attributes, update constraints

--- seed.py
+++ seed.py
@@ -28,24 +28,24 @@ def seed_login():
 
 def seed_users():
     users = [
-        {'username': 11111111, 'role_id': 1, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0},
-        {'username': 22222222, 'role_id': 2, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0},
-        {'username': 33333333, 'role_id': 3, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0},
-        {'username': 44444444, 'role_id': 4, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0},
+        {'username': 11111111, 'role_id': 1, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0, 'dept_id': 1},
+        {'username': 22222222, 'role_id': 2, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0, 'dept_id': 2},
+        {'username': 33333333, 'role_id': 3, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0, 'dept_id': 3},
+        {'username': 44444444, 'role_id': 4, 'leave_hours': 0.0, 'contract_hour': 40.0, 'available_hours': 40.0, 'dept_id': 1},
     ]
 
     for user in users:
-        new_user = User(username=user['username'], role_id=user['role_id'], leave_hours=user['leave_hours'], contract_hour=user['contract_hour'], available_hours=user['available_hours'])
+        new_user = User(username=user['username'], role_id=user['role_id'], leave_hours=user['leave_hours'], contract_hour=user['contract_hour'], available_hours=user['available_hours'], dept_id=user['dept_id'])
         db.session.add(new_user)
 
 def seed_work():
     work_types = ['ADMIN', 'CWS', 'GA', 'HDR', 'ORES', 'RES-MGMT', 'RESERV', 'SDS', 'TEACH', 'UDEV']
     for work_type in work_types:
-        work_example = Work(work_explanation=f"Some explanation for {work_type}", work_type=work_type, dept_id=1)  # Assuming `dept_id=1` for simplicity.
+        work_example = Work(work_explanation=f"Some explanation for {work_type}", work_type=work_type, dept_id=1, unit_code="if there is a unit code")
         db.session.add(work_example)
 
 def seed_workload_allocation():
-    workload = WorkloadAllocation(work_id=1, hours_allocated=4.0, username=33333333, comment="Some comment", comment_status="Unread")
+    workload = WorkloadAllocation(work_id=1, hours_allocated=4.0, username=33333333, comment="Some comment", comment_status="Unread", workload_point=10)
     db.session.add(workload)
 
 def run_seed():
