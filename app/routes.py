d28df65bba4bdb9fc85ea6837402ac7293adda3f
Kyle Leung <lkkc0300@gmail.com>
2023-09-16 23:52:50 +0800

    Added a conditional notification pop-up for HoDs in nav bar

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, request, redirect, url_for, flash, jsonify, current_app, session
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db, forms
-from app.models import Login
+from app.models import Login, User, WorkloadAllocation, Role
 from app.forms import LoginForm, SignupForm
 from werkzeug.urls import url_parse
 import re
 import random
+from flask import g
 
 # decorator for login page
 
@@ -29,17 +30,22 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 # decorator for logout page
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
 # decorator for view_workload page
+
+
 @app.route('/view_workload')
 @login_required
 def view_workload():
-    # users=User.query.all() # Just using user table for mockup 
-    return render_template('view_workload.html', title='View Workload')#, users=users)
+    # users=User.query.all() # Just using user table for mockup
+    # , users=users)
+    return render_template('view_workload.html', title='View Workload')
 
 
 @app.route('/assign_workload')
@@ -47,16 +53,77 @@ def view_workload():
 def assign():
     return render_template('assign_workload.html', title='Assign Workload')
 
+
 @app.route('/edit_allocation_detail')
 @login_required
 def edit_allocation_detail():
     return render_template('edit_allocation_detail.html', title='Edit Allocation Detail')
+
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
     return render_template('dashboard.html', title='Dashboard')
 
+
+# This function runs before processing a request
+@app.before_request
+def before_request():
+    # Initialise the global notification flag to False
+    g.show_notification = False
+    # Check if the current user is authenticated
+    if current_user.is_authenticated:
+        # Fetch the user data from the User table based on the current user's username
+        user = User.query.filter_by(username=current_user.username).first()
+
+        # If the user is found
+        if user:
+            # Check if the user's role is "HoD"
+            is_hod = user.role_id == Role.query.filter_by(
+                role_name='HoD').first().role_id
+
+            # Set a global variable to identify if the current user is an HoD
+            g.is_hod = is_hod
+
+            # If the user is an HoD
+            if is_hod:
+                # Query for unread comments from users in the same department as the HoD
+                unread_comments = WorkloadAllocation.query.join(User, WorkloadAllocation.username == User.username).filter(
+                    WorkloadAllocation.comment_status == "Unread",
+                    User.dept_id == user.dept_id
+                ).count()
+
+                # If there are any unread comments, set the global notification flag to True
+                g.show_notification = unread_comments > 0
+            else:
+                # If user is not an HoD, set the global notification flag to False
+                g.show_notification = False
+        else:
+            # If user data is not found in the User table, set the global notification flag to False
+            g.show_notification = False
+
+# Check for unread comments
+
+
+@app.route('/check_unread_comments')
+@login_required  # Ensure that the user is logged in to access this route
+def check_unread_comments():
+    # Fetch the user data from the User table based on the current user's username
+    user = User.query.filter_by(username=current_user.username).first()
+
+    # If the user is found
+    if user:
+        # Query for unread comments from users in the same department as the current user
+        unread_comments = WorkloadAllocation.query.join(User, WorkloadAllocation.username == User.username).filter(
+            WorkloadAllocation.comment_status == "Unread",
+            User.dept_id == user.dept_id
+        ).count()
+    else:
+        # If user data is not found, set unread comments to 0
+        unread_comments = 0
+
+    # Return the number of unread comments as a JSON response
+    return jsonify(unread_comments=unread_comments)
 
 
 if __name__ == '__main__':
